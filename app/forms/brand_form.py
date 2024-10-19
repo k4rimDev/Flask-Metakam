22cb298c2d5512c803060d306f1f16d74858e534
k4rimdev <karimmirzaguliyev@gmail.com>
2024-10-19 21:05:03 +0400

    feat: fix form problem, more beauty

--- app/forms/brand_form.py
+++ app/forms/brand_form.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 
-from wtforms import StringField, FileField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class BrandForm(FlaskForm):
-    logo = FileField('Logo')
+    logo = StringField('Logo Url')
     name = StringField('Name', validators=[DataRequired()])
     description = TextAreaField('Description')
     internal_id = StringField('Internal ID', validators=[DataRequired()])
